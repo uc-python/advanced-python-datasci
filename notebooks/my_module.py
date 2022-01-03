81c6c39b955c68208c5e289ab785edd5871f1a6b
Ethan Swan <eswan18@rocketmail.com>
2022-01-02 21:33:36 -0600

    Finish 07 and rename to Modularity Part 2.

--- notebooks/my_module.py
+++ notebooks/my_module.py
@@ -17,17 +17,18 @@ def get_features_and_target(csv_file, target_col):
     
     return (features, target)
 
-def make_preprocessor(features):
+def make_preprocessor(features, categorical_preprocessor=None, numeric_preprocessor=None):
     '''Create a column transformer that applies sensible preprocessing procedures.'''
-    categorical_preprocessor = OneHotEncoder(handle_unknown="ignore")
-    numeric_preprocessor = StandardScaler()
     
+    if categorical_preprocessor is None:
+        categorical_preprocessor = OneHotEncoder(handle_unknown='ignore')
+    if numeric_preprocessor is None:
+        numeric_preprocessor = StandardScaler()
+        
     numeric_columns = features.select_dtypes(exclude=object).columns
     categorical_columns = features.select_dtypes(include=object).columns
-
     preprocessor = ColumnTransformer([
         ('one-hot-encoder', categorical_preprocessor, categorical_columns),
         ('standard_scaler', numeric_preprocessor, numeric_columns)
     ])
-    
     return preprocessor
